28968903d993a1d7da480d81d8cdb458e5469106
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-03-25 20:17:25 +0100

    Update createPayment.js

--- api/createPayment.js
+++ api/createPayment.js
@@ -43,7 +43,6 @@ export default async function handler(req, res) {
     return res.status(200).json({
       checkoutUrl: data._links.checkout.href
     });
-
   } catch (error) {
     return res.status(500).json({
       error: 'Server error',
